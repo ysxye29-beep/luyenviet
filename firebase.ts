6f8c6a343190c648068f17b0ae61b3d7052bb327
kili <ysxye29@gmail.com>
2026-04-26 17:44:05 +0700

    Set custom parameters for GoogleAuthProvider

--- firebase.ts
+++ firebase.ts
@@ -7,6 +7,7 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 
 export enum OperationType {
   CREATE = 'create',
